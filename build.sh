2e37e3422040068eb334d4e1228451d7c14f2614
Natanael Arndt <arndtn@gmail.com>
2024-11-04 16:18:02 +0100

    Add some packages

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install tmux syncthing python-3-virtualenvwrapper pipx neovim keepassxc hedgewars
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
